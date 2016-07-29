88d03fe8768f3ca57b092ee9c1fe9fb9af446bad
Mackenzie Meade <mcquadzie@gmail.com>
2016-07-28 19:06:59 -0700

    Added both .ino files, not sure what CylonHat2.ino is for

---
+++ CylonHat.ino
@@ -0,0 +1,85 @@
+    // Source: https://learn.adafruit.com/larson-scanner-shades/software
+
+    #include <Adafruit_NeoPixel.h>
+     
+    #define N_LEDS 24
+    #define PIN 1
+     
+    // Parameter 1 = number of pixels in strip
+    // Parameter 2 = pin number (most are valid)
+    // Parameter 3 = pixel type flags, add together as needed:
+    // NEO_KHZ800 800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+    // NEO_KHZ400 400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+    // NEO_GRB Pixels are wired for GRB bitstream (most NeoPixel products)
+    // NEO_RGB Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+    Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+     
+    void setup() {
+    strip.begin();
+    strip.setBrightness(75);
+    strip.show();
+    }
+     
+    int pos = 0, dir = 1; // Position, direction of "eye"
+    int mode = 1;
+     
+    void loop() {
+    int j;
+
+
+    if (mode == 1)
+    {
+      //    
+      //               Rainbow mode
+      //
+        if (dir < 0)    // If dir is positive draw red -> purple
+        {
+            strip.setPixelColor(pos - 2, 255, 0, 0);
+            strip.setPixelColor(pos - 1, 255, 85, 0);
+            strip.setPixelColor(pos, 255, 255, 0);
+            strip.setPixelColor(pos + 1, 0, 128, 0);
+            strip.setPixelColor(pos + 2, 0, 0, 255);
+            strip.setPixelColor(pos + 3, 128, 0, 128);
+        }
+        else            // If dir is negative, draw purple -> red
+        {
+            strip.setPixelColor(pos - 2, 128, 0, 128);
+            strip.setPixelColor(pos - 1, 0, 0, 255);
+            strip.setPixelColor(pos, 0, 128, 0);
+            strip.setPixelColor(pos + 1, 255, 255, 0);
+            strip.setPixelColor(pos + 2, 255, 165, 0);
+            strip.setPixelColor(pos + 3, 255, 0, 0);
+        }
+    }
+    else
+    {
+      //
+      //              Cylon mode
+      //
+
+        strip.setPixelColor(pos - 3, 20, 0, 0);
+        strip.setPixelColor(pos - 2, 80, 0, 0);
+        strip.setPixelColor(pos - 1, 165, 0, 0);
+        strip.setPixelColor(pos, 255, 0, 0);
+        strip.setPixelColor(pos + 1, 165, 0, 0);
+        strip.setPixelColor(pos + 2, 80, 0, 0);
+        strip.setPixelColor(pos + 3, 20, 0, 0);
+    }
+    
+    strip.show();
+    delay(40);
+     
+    // Rather than being sneaky and erasing just the tail pixel,
+    // it's easier to erase it all and draw a new one next time.
+    for(j=-3; j<= 3; j++) strip.setPixelColor(pos+j, 0);
+     
+    // Bounce off ends of strip
+    pos += dir;
+    if(pos < 0) {
+    pos = 1;
+    dir = -dir;
+    } else if(pos >= strip.numPixels() - 2) {
+    pos = strip.numPixels() - 4;
+    dir = -dir;
+    }
+    }
---
+++ CylonHat2.ino
@@ -0,0 +1,42 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 1 //Special requirement, what is this? 
+#define NUM_LIGHTS 26//Defines the total number of LEDs
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LIGHTS, PIN, NEO_GRB + NEO_KHZ800); //Defines a NeoPixel opject, our strip of lights. Parameters = number of lights, pin #, and special color code thing
+
+void setup()
+{
+ strip.begin(); //Prepares strip for data, ***use strip.show() to initialize all pixels to off***
+ strip.show(); 
+}
+
+void loop() 
+{
+  //Test to set a pixel color- arguments are: (LED #, red, green, blue)
+  strip.setPixelColor(0, 255, 0, 255); //Sets first LED (at position 0) to magenta (all red and blue) 
+
+  strip.show(); 
+
+  strip.setBrightness(100); //set brightness lower
+  
+  colorWipe(0, 100);
+  
+ 
+}
+
+ void colorWipe(uint32_t c, uint8_t wait) 
+  {
+    for(uint16_t i=0; i < NUM_LIGHTS; i++) 
+    {
+      strip.setPixelColor(i - 2, 0x100000); // Dark red
+      strip.setPixelColor(i - 1, 0x800000); // Medium red
+      strip.setPixelColor(i, 0xFF3000); // Center pixel is brightest
+      strip.setPixelColor(i + 1, 0xFF3000); // Center pixel is brightest
+      strip.setPixelColor(i + 2, 0x800000); // Medium red
+      strip.setPixelColor(i + 3, 0x100000); // Dark red
+      strip.show();
+      for(int j=-3; j<= 3; j++) strip.setPixelColor(i+j, 0);
+      delay(wait);
+    }
+  }
